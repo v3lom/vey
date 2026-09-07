48486e4632758f55c9a6fb992cab5160f40aab30
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2026-09-07 17:26:59 +0300

    changes

--- src/inet/inet.cpp
+++ src/inet/inet.cpp
@@ -4,6 +4,12 @@
 
 
 static bool Verbose;
+static bool is_v4;
+static bool is_v6;
+static bool is_tcp;
+static bool is_udp;
+static std::pair<bool, unsigned long long> reconnectCount;
+static std::pair<bool, unsigned long long> writeDelay;
 
 namespace
 {
@@ -22,12 +28,18 @@ namespace
                                        std::cerr << "Connect error for " << Endpoint << ": " << Er.message()
                                                  << std::endl;
 
+                                       if (reconnectCount.first)
+                                       {
+                                           if (reconnectCount.second == count++)
+                                               return;
+                                       }
 
                                        if (Er == boost::asio::error::connection_refused)
                                        {
                                            std::cerr << "Reconnecting..." << std::endl;
                                            connect(Buff);
                                        }
+
 
                                        return;
                                    }
@@ -53,6 +65,11 @@ namespace
                                 if (Verbose)
                                     std::cout << "Sent: " << Size << " bytes" << std::endl;
 
+                                if (writeDelay.first)
+                                {
+                                    std::this_thread::sleep_for(std::chrono::milliseconds(writeDelay.second));
+                                }
+
                                 write(Buff);
                             });
         }
@@ -61,13 +78,14 @@ namespace
         boost::asio::io_context& Io;
         SockType Sock;
         EndPointType Endpoint;
+        unsigned long long count = 0;
     };
 
 } // namespace
 
 namespace inet
 {
-    static auto getNeedEndpoint(const auto& Endpoints, bool is_v4, bool is_v6)
+    static auto getNeedEndpoint(const auto& Endpoints)
     {
         for (const auto& Endpoint : Endpoints)
         {
@@ -82,14 +100,14 @@ namespace inet
         throw std::runtime_error("Resolver not found target");
     }
 
-    static int tcpva(const argparse::ArgumentParser& Program, bool is_v4, bool is_v6)
+    static int tcpva(const argparse::ArgumentParser& Program)
     {
         boost::asio::io_context Io;
 
         boost::asio::ip::tcp::resolver Resolver(Io);
         boost::asio::ip::tcp::resolver::results_type Endpoints =
             Resolver.resolve(Program.get("ip"), Program.get("port"));
-        boost::asio::ip::tcp::resolver::endpoint_type Endpoint = getNeedEndpoint(Endpoints, is_v4, is_v6);
+        boost::asio::ip::tcp::resolver::endpoint_type Endpoint = getNeedEndpoint(Endpoints);
 
         std::vector Buffer(Program.get<unsigned long long>("-p"), 'X');
         std::vector<Sender<boost::asio::ip::tcp::socket, boost::asio::ip::tcp::resolver::endpoint_type>> Clients;
@@ -105,14 +123,14 @@ namespace inet
         return 0;
     }
 
-    static int udpva(const argparse::ArgumentParser& Program, bool is_v4, bool is_v6)
+    static int udpva(const argparse::ArgumentParser& Program)
     {
         boost::asio::io_context Io;
 
         boost::asio::ip::udp::resolver Resolver(Io);
         boost::asio::ip::udp::resolver::results_type Endpoints =
             Resolver.resolve(Program.get("ip"), Program.get("port"));
-        boost::asio::ip::udp::resolver::endpoint_type Endpoint = getNeedEndpoint(Endpoints, is_v4, is_v6);
+        boost::asio::ip::udp::resolver::endpoint_type Endpoint = getNeedEndpoint(Endpoints);
 
         std::vector Buffer(Program.get<unsigned long long>("-p"), 'X');
         std::vector<Sender<boost::asio::ip::udp::socket, boost::asio::ip::udp::resolver::endpoint_type>> Clients;
@@ -131,16 +149,25 @@ namespace inet
 
     int inet(const argparse::ArgumentParser& Program)
     {
-        bool is_v4 = Program.get<bool>("-v4");
-        bool is_v6 = Program.get<bool>("-v6");
-        bool is_tcp = Program.get<bool>("-t");
-        bool is_udp = Program.get<bool>("-u");
+        is_v4 = Program.get<bool>("-v4");
+        is_v6 = Program.get<bool>("-v6");
+        is_tcp = Program.get<bool>("-t");
+        is_udp = Program.get<bool>("-u");
         Verbose = Program.get<bool>("--verbose");
+        reconnectCount.first = Program.present<unsigned long long>("-rc").has_value();
+        writeDelay.first = Program.present<unsigned long long>("-dw").has_value();
+
+        if (reconnectCount.first)
+            reconnectCount.second = Program.get<unsigned long long>("-rc");
+
+        if (writeDelay.first)
+            writeDelay.second = Program.get<unsigned long long>("-dw");
+
 
         if (is_tcp)
-            return tcpva(Program, is_v4, is_v6);
+            return tcpva(Program);
         if (is_udp)
-            return udpva(Program, is_v4, is_v6);
+            return udpva(Program);
 
         std::cerr << Program;
         return 1;
--- src/vey/main.cpp
+++ src/vey/main.cpp
@@ -11,6 +11,12 @@ int main(int argc, char* argv[])
     InetCmd.add_argument("--verbose").help("enable detailed logging / verbose output").flag();
     InetCmd.add_argument("ip").help("target IP address (IPv4 or IPv6)");
     InetCmd.add_argument("port").help("target port number (1-65535)");
+    InetCmd.add_argument("-rc", "--reconnect-count")
+        .help("maximum number of reconnection attempts [default: no reconnect]")
+        .scan<'u', unsigned long long>();
+    InetCmd.add_argument("-dw", "--delay-ms-per-write")
+        .help("delay between consecutive write operations in milliseconds [default: no delay]")
+        .scan<'u', unsigned long long>();
 
     InetCmd.add_argument("-s", "--socket-count")
         .help("number of concurrent sockets to use")
